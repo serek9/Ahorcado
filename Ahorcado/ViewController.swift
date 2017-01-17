2ea3dfb611d55763baa4cdfb34a27eb4949f30ca
DAM <sergiodiaz808@gmail.com>
2017-01-17 20:12:19 +0100

    Initial Commit

---
+++ Ahorcado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ahorcado
+//
+//  Created by DAM on 17/1/17.
+//  Copyright © 2017 DAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
